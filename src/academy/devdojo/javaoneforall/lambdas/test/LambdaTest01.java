d9d6575dffdf73d4ceb1a5e59e3e30a72c41b79d
Caio-Paulino <10paulinhosilva10@gmail.com>
2026-01-19 19:24:01 -0300

    feat: add lambdas

---
+++ src/academy/devdojo/javaoneforall/lambdas/test/LambdaTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.javaoneforall.lambdas.test;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class LambdaTest01 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("Caio", "Paulino", "Goku");
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        forEach(strings, (String s) -> System.out.println(s) );
+        forEach(integers, (Integer i) -> System.out.println(i) );
+    }
+
+    private static <T> void forEach(List<T> list, Consumer<T> consumer) {
+        for (T e : list) {
+            consumer.accept(e);
+        }
+
+    }
+}
